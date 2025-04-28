2abe5788a45f64dbc7235c215bef37af2c7742f4
Noah <pro@noahcode.dev>
2025-04-28 23:58:25 +0200

    fix: width of task

--- templates/partials/sheet_templ.go
+++ templates/partials/sheet_templ.go
@@ -222,7 +222,7 @@ func ExerciseContent(content string) templ.Component {
 			templ_7745c5c3_Var9 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 15, "<div class=\"md:overflow-y-auto md:grow prose\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 15, "<div class=\"md:overflow-y-auto md:grow prose max-w-none\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
